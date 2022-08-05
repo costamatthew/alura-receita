dd9739e4ce7df5d748bef0b84c1cdde24d27b7bc
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-08-04 22:59:52 -0300

    feat

--- receitas/views.py
+++ receitas/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>RECEITAS</h1>')
+    return render(request,'index.html')
+
+def receita(request):
+    return render(request,'receita.html')
